24ffc815d4ef9a0d584372302a2e1df8fbd76bb7
aliabdollahi <you@example.com>
2017-12-11 00:21:49 +0330

    ShortestJobFirst.cc initial class methods

--- threads/ShortestJobFirst.cc
+++ threads/ShortestJobFirst.cc
@@ -1,15 +1,11 @@
 #include "copyright.h"
-#include "scheduler.h"
+#include "ShortestJobFirst.h"
 #include "system.h"
 
 ShortestJobFirst::ShortestJobFirst()
 { 
     readyList = new List; 
 
-    int i;
-    for(i=0; i<MAX_THREAD_COUNT; ++i) {
-        cpu_count[i] = 0;
-    }
 } 
 
 
@@ -22,27 +18,7 @@ void
 ShortestJobFirst::ReadyToRun (Thread *thread)
 {
     thread->setStatus(READY);
-    thread->wait_time_start = stats->totalTicks;
-    if(thread->cpu_burst_previous >0 ){
-        
-        double estimate = alpha*thread->cpu_burst_previous 
-                        + ( 1- alpha )*thread->cpu_burst_estimate;
-
-        
-        if(thread->cpu_burst_previous > 0) {
-            int error;
-            error = thread->cpu_burst_previous - estimate;
-            if (error < 0 ) {
-                error = error * -1;
-            }
-
-            stats->estimate_error += error;
-        }
-
-        thread->cpu_burst_estimate = estimate;
-        readyList->SortedInsert((void *)thread, estimate);
-    } else {
-        readyList->Append((void *)thread);
+    readyList->Append((void *)thread);
 }
 
 Thread *ShortestJobFirst::FindNextToRun ()
@@ -56,38 +32,9 @@ ShortestJobFirst::Run (Thread *nextThread)
 {
     Thread *oldThread = currentThread;
     
-#ifdef USER_PROGRAM			
-    if (currentThread->space != NULL) {	
-        currentThread->SaveUserState(); 
-	currentThread->space->SaveState();
-    }
-#endif
     
     oldThread->CheckOverflow();		    
 					  
-    oldThread->total_time += stats->totalTicks - oldThread->start_time;
-
-    currentThread = nextThread;		    
-    currentThread->setStatus(RUNNING);    
-  
-    nextThread->cpu_burst_start = stats->totalTicks;
-    nextThread->start_time = stats->totalTicks;
-    nextThread->wait_time += stats->totalTicks - nextThread->wait_time_start;
-
-    _SWITCH(oldThread, nextThread);
-
-
-    if (threadToBeDestroyed != NULL) {
-        delete threadToBeDestroyed;
-	threadToBeDestroyed = NULL;
-    }
-    
-#ifdef USER_PROGRAM
-    if (currentThread->space != NULL) {
-        currentThread->RestoreUserState();     
-	currentThread->space->RestoreState();
-    }
-#endif
 }
 
 
